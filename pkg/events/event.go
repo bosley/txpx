ba10bce15e57164a75a9011727c5f78c176660c5
bosley <josh.bosley@proton.me>
2025-09-26 13:54:43 -0400

    m

--- pkg/events/event.go
+++ pkg/events/event.go
@@ -9,8 +9,10 @@ and use within their specific context, and can not be overridden by the applicat
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log/slog"
 	"sync"
+	"sync/atomic"
 
 	"github.com/bosley/txpx/pkg/pool"
 )
@@ -22,7 +24,8 @@ var (
 
 const (
 	MaxLocalEventTopics     = 2048
-	TotalEventSystemWorkers = 256 // we can have a lot of these so we need to limit them
+	MaxHandlers             = 1024 // max handlers per topic
+	TotalEventSystemWorkers = 256  // we can have a lot of these so we need to limit them
 )
 
 const (
@@ -40,6 +43,7 @@ const (
 var _openTopic = TopicUsageSentinel
 var _availableTopicCounter int
 var _availableTopicCounterMu sync.Mutex
+var _handlerIDCounter int64
 
 func init() {
 	_openTopic = TopicUsageSentinel
@@ -61,15 +65,19 @@ type Event struct {
 	Body   interface{}
 }
 
-type subscriber struct {
-	subscriberUUID string
-	topic          int
-	channel        chan Event
+type EventHandler interface {
+	OnEvent(event Event)
+}
+
+type handler struct {
+	handlerUUID string
+	topic       int
+	callback    EventHandler
 }
 
 type topicData struct {
-	subscribers []subscriber
-	mu          sync.Mutex
+	handlers []handler
+	back     int32 // atomic access - current count of valid handlers
 }
 
 type LocalEventSystem struct {
@@ -96,10 +104,12 @@ var _ TopicPublisher = &topicPublisherImpl{}
 // the topic publisher is not changed and we can ensure as much movement as possible.
 func (tpi *topicPublisherImpl) Publish(event Event) {
 	// We dont lock because we explicitly dont allow deletion and only grow monotonically in indexing
-	// so there is no race condition
-	for _, subscriber := range tpi.les.topics[tpi.topic].subscribers {
+	// so there is no race condition. We use atomic back counter for bounds checking.
+	back := atomic.LoadInt32(&tpi.les.topics[tpi.topic].back)
+	for i := int32(0); i < back; i++ {
+		h := tpi.les.topics[tpi.topic].handlers[i] // capture for closure
 		tpi.les.workerPool.Submit(func(ctx context.Context) error {
-			subscriber.channel <- event
+			h.callback.OnEvent(event)
 			return nil
 		})
 	}
@@ -111,8 +121,8 @@ func NewLocalEventSystem(logger *slog.Logger, ctx context.Context) *LocalEventSy
 	topics := make([]topicData, MaxLocalEventTopics)
 	for i := range topics {
 		topics[i] = topicData{
-			subscribers: make([]subscriber, 0),
-			mu:          sync.Mutex{},
+			handlers: make([]handler, MaxHandlers),
+			back:     0,
 		}
 	}
 
@@ -127,7 +137,7 @@ func NewLocalEventSystem(logger *slog.Logger, ctx context.Context) *LocalEventSy
 		"system-reserved6": TopicReserved6,
 	}
 
-	if len(knownTopics) != TopicUsageSentinel-1 {
+	if len(knownTopics) != TopicUsageSentinel {
 		panic("known topics does not match expected number of topics")
 	}
 
@@ -160,4 +170,31 @@ func (les *LocalEventSystem) GetTopicPublisher(name string) (TopicPublisher, err
 		les:   les,
 		topic: les.knownTopics[name],
 	}, nil
+}
+
+func (les *LocalEventSystem) AddHandler(topicName string, callback EventHandler) (string, error) {
+	topicID, ok := les.knownTopics[topicName]
+	if !ok {
+		return "", ErrTopicNotRegistered
+	}
+
+	handlerID := fmt.Sprintf("handler-%d", atomic.AddInt64(&_handlerIDCounter, 1))
+
+	// Atomically reserve a slot by incrementing back
+	slot := atomic.AddInt32(&les.topics[topicID].back, 1) - 1
+	if slot >= MaxHandlers {
+		// This should never happen in normal operation, but safety check
+		atomic.AddInt32(&les.topics[topicID].back, -1) // rollback
+		panic("max handlers per topic reached")
+	}
+
+	h := handler{
+		handlerUUID: handlerID,
+		topic:       topicID,
+		callback:    callback,
+	}
+
+	les.topics[topicID].handlers[slot] = h
+
+	return handlerID, nil
 }
